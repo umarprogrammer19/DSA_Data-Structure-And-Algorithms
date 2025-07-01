0f1dd3204fe15cebad1ea43a63645eaf00a208a6
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-01 14:33:38 +0500

    Largest Number

--- 01-Basic-Problem-Practices/Number-based-Problems.py
+++ 01-Basic-Problem-Practices/Number-based-Problems.py
@@ -1,6 +1,17 @@
 # Check if a number is even or odd.
 
-# n = int(input("Enter A Number: "))
-# even = "Even Number" if n % 2 == 0 else "Odd Number"
-# print(even)
+n = int(input("Enter A Number: "))
+even = "Even Number" if n % 2 == 0 else "Odd Number"
+print(even)
 
+# Find the largest of three numbers.
+
+num2 = input("Enter 3 Numbers: ").split()
+val = [int(c) for c in num2]
+largest = val[0]
+
+for value in val:
+    if value > largest:
+        largest = value
+
+print(largest)
