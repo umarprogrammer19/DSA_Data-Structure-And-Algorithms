84ec0367ec686dfd461c262dce5cbf148090384a
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-01 14:50:41 +0500

    Prime Number Calculate

--- 01-Basic-Problem-Practices/Number-based-Problems.py
+++ 01-Basic-Problem-Practices/Number-based-Problems.py
@@ -1,23 +1,40 @@
 # Check if a number is even or odd.
 
-n = int(input("Enter A Number: "))
-even = "Even Number" if n % 2 == 0 else "Odd Number"
-print(even)
+# n = int(input("Enter A Number: "))
+# even = "Even Number" if n % 2 == 0 else "Odd Number"
+# print(even)
 
 # Find the largest of three numbers.
-from functools import reduce
+# from functools import reduce
 
-num2 = input("Enter 3 Numbers: ").split()
-val = [int(c) for c in num2]
+# num2 = input("Enter 3 Numbers: ").split()
+# val = [int(c) for c in num2]
 
-largest = reduce(lambda x, y: x if x > y else y, val)  # for one line answer
-print(largest)
+# largest = reduce(lambda x, y: x if x > y else y, val)  # for one line answer
+# print(largest)
 
 # For Normal
-largest = val[0]
+# largest = val[0]
 
-for value in val:
-    if value > largest:
-        largest = value
+# for value in val:
+#     if value > largest:
+#         largest = value
 
-print(largest)
+# print(largest)
+
+# Check if a number is prime.
+num3 = 5
+flag = False
+
+if num3 == 0 or num3 == 1:
+    print("Number is not prime")
+elif num3 > 1:
+    for i in range(2, num3):
+        if (num3 % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num3, "is not a prime number")
+else:
+    print(num3, "is a prime number")
