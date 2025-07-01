048bd57fcb07a66f87aa11feda7f1def39b29d1f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-01 14:37:53 +0500

    Largest Finder

--- 01-Basic-Problem-Practices/Number-based-Problems.py
+++ 01-Basic-Problem-Practices/Number-based-Problems.py
@@ -5,9 +5,15 @@ even = "Even Number" if n % 2 == 0 else "Odd Number"
 print(even)
 
 # Find the largest of three numbers.
+from functools import reduce
 
 num2 = input("Enter 3 Numbers: ").split()
 val = [int(c) for c in num2]
+
+largest = reduce(lambda x, y: x if x > y else y, val)  # for one line answer
+print(largest)
+
+# For Normal
 largest = val[0]
 
 for value in val:
