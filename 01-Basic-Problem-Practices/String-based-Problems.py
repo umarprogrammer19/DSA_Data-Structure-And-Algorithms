ee8baa2df4a23778f736f6cb7777bb92c09f0253
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-02 11:25:44 +0500

    Vowel And Cons

--- 01-Basic-Problem-Practices/String-based-Problems.py
+++ 01-Basic-Problem-Practices/String-based-Problems.py
@@ -1,6 +1,20 @@
 # Check if a string is a palindrome.
-string = input()
+string = input("Enter a string to check a palindrome: ")
 if string == string[::-1]:
     print(string, "is Palindrome")
 else:
     print(string, "is not Palindrome")
+
+# Count vowels and consonants in a string.
+string2 = input("Enter a string to count the vowels and consonants: ")
+vowels = ["a", "e", "i", "o", "u"]
+vov, cons = 0, 0
+
+for val in string2:
+    if val.lower() in vowels:
+        vov += 1
+    elif val.isalpha():
+        cons += 1
+
+print("Vowels:", vov)
+print("Consonants:", cons)
