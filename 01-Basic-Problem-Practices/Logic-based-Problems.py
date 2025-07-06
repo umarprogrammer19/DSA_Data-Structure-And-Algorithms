d5e9ee750d53f4695247effe34dc3417bb08fc1b
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-07-06 22:21:29 +0500

    Table

--- 01-Basic-Problem-Practices/Logic-based-Problems.py
+++ 01-Basic-Problem-Practices/Logic-based-Problems.py
@@ -4,3 +4,7 @@ b = 8
 a, b = b, a
 print(a, b)
 
+# Print multiplication table of a number.
+n = int(input("Enter a number to print a table: "))
+for i in range(1, 11):
+    print(f"{n} x {i} = {n*i}")
