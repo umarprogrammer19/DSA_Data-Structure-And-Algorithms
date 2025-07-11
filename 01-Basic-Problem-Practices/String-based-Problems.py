51dd2db6b6c399a591bbffb5f1d1a7284f3d2add
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-11 11:43:10 +0500

    Updated

--- 01-Basic-Problem-Practices/String-based-Problems.py
+++ 01-Basic-Problem-Practices/String-based-Problems.py
@@ -1,74 +1,74 @@
-# # Check if a string is a palindrome.
-# string = input("Enter a string to check a palindrome: ")
-# if string == string[::-1]:
-#     print(string, "is Palindrome")
-# else:
-#     print(string, "is not Palindrome")
+# Check if a string is a palindrome.
+string = input("Enter a string to check a palindrome: ")
+if string == string[::-1]:
+    print(string, "is Palindrome")
+else:
+    print(string, "is not Palindrome")
 
-# # Count vowels and consonants in a string.
-# s = input("Enter a string to count the vowels and consonants: ")
-# vowels = ["a", "e", "i", "o", "u"]
-# vov, cons = 0, 0
+# Count vowels and consonants in a string.
+s = input("Enter a string to count the vowels and consonants: ")
+vowels = ["a", "e", "i", "o", "u"]
+vov, cons = 0, 0
 
-# for val in s:
-#     if val.lower() in vowels:
-#         vov += 1
-#     elif val.isalpha():
-#         cons += 1
+for val in s:
+    if val.lower() in vowels:
+        vov += 1
+    elif val.isalpha():
+        cons += 1
 
-# print("Vowels:", vov)
-# print("Consonants:", cons)
+print("Vowels:", vov)
+print("Consonants:", cons)
 
-# # Find the frequency of each character in a string.
-# s = input("Enter a string to find the frequency in it: ")
-# freq = {}
+# Find the frequency of each character in a string.
+s = input("Enter a string to find the frequency in it: ")
+freq = {}
 
-# # Simple Method
-# for c in s:
-#     if c in freq:
-#         freq[c] += 1
-#     else:
-#         freq[c] = 1
-# print(freq)
+# Simple Method
+for c in s:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+print(freq)
 
-# # With Counter
-# from collections import Counter
+# With Counter
+from collections import Counter
 
-# freq = Counter(s)
-# print(dict(freq))
+freq = Counter(s)
+print(dict(freq))
 
-# # Dictionary Comprehension
-# freq = {c: s.count(c) for c in s}
-# print(freq)
+# Dictionary Comprehension
+freq = {c: s.count(c) for c in s}
+print(freq)
 
-# # Reverse a string without using built-in functions.
-# s = input("Enter a string to reverse string: ")
-# rev = ""
-# for c in s:
-#     rev = c + rev
-# print(rev)
+# Reverse a string without using built-in functions.
+s = input("Enter a string to reverse string: ")
+rev = ""
+for c in s:
+    rev = c + rev
+print(rev)
 
-# # Capitalize the first letter of each word in a string.
-# s = input("Enter a string to Capitalize 1st car in string: ")
-# print(" ".join([c.capitalize() for c in s.strip().split()]))
-# print(s.title())
+# Capitalize the first letter of each word in a string.
+s = input("Enter a string to Capitalize 1st car in string: ")
+print(" ".join([c.capitalize() for c in s.strip().split()]))
+print(s.title())
 
-# # Count the number of words in a sentence.
-# s = input("Enter a string to find a number of words in it ")
-# print(len(s.split()))
+# Count the number of words in a sentence.
+s = input("Enter a string to find a number of words in it ")
+print(len(s.split()))
 
-# # Replace all spaces in a string with hyphens (-).
-# s = input("Enter a string to join string with -: ")
-# print(s.replace(" ", "-"))
+# Replace all spaces in a string with hyphens (-).
+s = input("Enter a string to join string with -: ")
+print(s.replace(" ", "-"))
 
-# # Remove all special characters from a string.
-# import string
+# Remove all special characters from a string.
+import string
 
-# s = input("Enter a string to remove all the special characters in it: ")
-# final_string_without_special_characters = "".join(
-#     char for char in s if char not in string.punctuation
-# )
-# print(final_string_without_special_characters)
+s = input("Enter a string to remove all the special characters in it: ")
+final_string_without_special_characters = "".join(
+    char for char in s if char not in string.punctuation
+)
+print(final_string_without_special_characters)
 
 # Find the longest word in a sentence.
 s = input("Enter a string to find the longest word in it: ")
