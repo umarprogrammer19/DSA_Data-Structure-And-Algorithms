23b57253fb889fbf161366b63bc77c003b3b8037
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-01-01 23:42:14 +0500

    Method And Algorithm

--- 03-Daily-Algorithm-Challenges/01-PlusOne.py
+++ 03-Daily-Algorithm-Challenges/01-PlusOne.py
@@ -1,5 +1,8 @@
 # Plus One (1st January 2026) Leet Code Challenge
 
+# Method Name: String Conversion Method
+# Algorithm Type: Simulation / Direct Conversion Approach
+
 # You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
 # Increment the large integer by one and return the resulting array of digits.
