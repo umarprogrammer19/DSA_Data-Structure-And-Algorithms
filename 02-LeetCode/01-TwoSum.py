7f8c801689b4aea7fc14f506ef466971a790c306
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-01-01 23:09:13 +0500

    Explanation Added

--- 02-LeetCode/01-TwoSum.py
+++ 02-LeetCode/01-TwoSum.py
@@ -47,3 +47,21 @@ print(solution.twoSum(nums=[2, 7, 11, 15], target=9))  # [0, 1]
 print(solution.twoSum(nums=[3, 2, 4], target=6))  # [1, 2]
 print(solution.twoSum(nums=[3, 3], target=6))  # [0, 1]
 
+# Explanation
+
+# There is a function called twoSum which takes two parameters: nums and target.
+# The Solution class is provided by default as boilerplate code in LeetCode. Inside this class, there is one method called twoSum.
+# In the twoSum method, I use two loops. The outer loop uses the variable i and iterates through the array starting from index 0. The inner loop uses the variable j and starts from i + 1, so the same element is not used twice.
+
+# Inside the inner loop, I check a condition:
+# if nums[i] + nums[j] is equal to the target, then I return the indices [i, j].
+# This way, the function finds and returns the indices of the two numbers whose sum is equal to the target.
+
+# Example
+
+# Input: nums = [2, 7, 11, 15], target = 9
+
+# First, i = 0, so nums[i] = 2 -> because index 0 has value which is 2 
+# Then j = 1, so nums[j] = 7 -> because index 1 has value which is 7
+# nums[0] + nums[1] = 2 + 7 = 9, which is equal to the target
+# Therefore, the function returns [0, 1]
