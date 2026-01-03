666c82459c8025b61fbbebf9d5548a769e4dc5c4
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-01-03 23:27:41 +0500

    Solved

--- 03-Daily-Algorithm-Challenges/03-number-of-ways-to-paint-Nx3-grid.py
+++ 03-Daily-Algorithm-Challenges/03-number-of-ways-to-paint-Nx3-grid.py
@@ -12,3 +12,22 @@
 
 # Input: n = 5000
 # Output: 30228214
+
+
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        MOD = 10**9 + 7
+
+        a, b = 6, 6
+
+        for _ in range(2, n + 1):
+            new_a = (3 * a + 2 * b) % MOD
+            new_b = (2 * a + 2 * b) % MOD
+            a, b = new_a, new_b
+
+        return (a + b) % MOD
+
+
+solution = Solution()
+print(solution.numOfWays(1))
+print(solution.numOfWays(5000))
