3a3bbc94c371e98379e5c09d209bfaaea97a3820
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-01-05 15:16:34 +0500

    Max Matrix Sum

---
+++ 03-Daily-Algorithm-Challenges/05-MaximumMatrixSum.py
@@ -0,0 +1,17 @@
+# 1975. Maximum Matrix Sum (5th Of January 2026)
+
+# You are given an n x n integer matrix. You can do the following operation any number of times:
+# Choose any two adjacent elements of matrix and multiply each of them by -1.
+# Two elements are considered adjacent if and only if they share a border.
+# Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+
+# Example 1:
+
+# 03-Daily-Algorithm-Challenges/problemset_diagrams/05-MaxMatrixSum1.png (see the image in diagrams folder)
+
+# Input: matrix = [[1,-1],[-1,1]]
+# Output: 4
+# Explanation: We can follow the following steps to reach sum equals 4:
+# - Multiply the 2 elements in the first row by -1.
+# - Multiply the 2 elements in the first column by -1.
+
