c62b3127397ee5674b6fe9bcef5475f6d684c227
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-01-10 22:54:46 +0500

    Diagram

--- 03-Daily-Algorithm-Challenges/06-Maximum-Level-Sum-Of-Binary-Tree.py
+++ 03-Daily-Algorithm-Challenges/06-Maximum-Level-Sum-Of-Binary-Tree.py
@@ -1,1 +1,7 @@
 # 1161. Maximum Level Sum of a Binary Tree (6th January 2026)
+
+# Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+# Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+# Example 1:
